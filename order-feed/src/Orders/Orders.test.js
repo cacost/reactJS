d383d0f3c3cec31f0eeccc48e109be8b40c187c7
Caio Costa <cacost94@gmail.com>
2020-02-05 14:15:46 -0300

    deleted test

--- order-feed/src/Orders/Orders.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Orders from '../Orders';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Orders />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
